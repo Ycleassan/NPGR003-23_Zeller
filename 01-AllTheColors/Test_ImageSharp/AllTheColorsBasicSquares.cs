35d2e56c7d764ca07ce439e636eb6c4c2dd2e5d1
Vojtěch Zeller <v.zell@seznam.cz>
2023-10-03 11:44:52 +0200

    Zakladni zpusob jak rozhazet 16M barev.

---
+++ 01-AllTheColors/Test_ImageSharp/AllTheColorsBasicSquares.cs
@@ -0,0 +1,28 @@
+﻿using SixLabors.ImageSharp;
+using System.Collections.Immutable;
+using static System.Net.Mime.MediaTypeNames;
+
+void GB_Square(byte red, int startX, int startY, Image<Rgba32> image)
+{
+  for (int i = 0; i < 256; i++)
+  {
+    for (int j = 0; j < 256; j++)
+    {
+      image[i + startX, j+startY] = new Rgba32(red, (byte)i, (byte)j);
+    }
+  }
+}
+
+using (var image = new Image<Rgba32>(256*16, 256*16))
+{
+  for(int i = 0; i < 16; ++i)
+  {
+    for (int j = 0; j < 16; ++j)
+    {
+      GB_Square((byte)(16*i + j), 256*i, 256*j, image);
+    }
+  }
+
+
+  image.SaveAsPng("..\\..\\..\\..\\16mColorsZeller.png");
+}
